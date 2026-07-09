f8914b98db005f162c86457a32227b1f0b0072b8
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-09 19:58:12 +0200

    Add initial HTML structure to style.css

---
+++ style.css
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Line Distribution</title>
+</head>
+<body>
+    <h1>Mi proyecto de Line Distribution</h1>
+</body>
+</html>
