f786612158cfb6cb04e6634b290943d8b7a54850
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-12 13:09:25 +0200

    Add configuration for app, UI, lyrics, and members

--- js/config.js
+++ js/config.js
@@ -1,1 +1,71 @@
+const CONFIG = {
 
+    app: {
+
+        group: "NCT DREAM",
+
+        song: "Moonlight"
+
+    },
+
+    ui: {
+
+        animationSpeed: 250,
+
+        progressBarHeight: 10,
+
+        memberPhotoSize: 52,
+
+        showVideo: true,
+
+        showSeconds: true
+
+    },
+
+    lyrics: {
+
+        fontSize: 30,
+
+        memberFontSize: 38,
+
+        showHangul: true,
+
+        showRomanized: true,
+
+        showEnglish: true
+
+    },
+
+    members: {
+
+        MARK: {
+            color: "#b353cd"
+        },
+
+        HAECHAN: {
+            color: "#0089b6"
+        },
+
+        RENJUN: {
+            color: "#ab842f"
+        },
+
+        CHENLE: {
+            color: "#008206"
+        },
+
+        JENO: {
+            color: "#cb0c0c"
+        },
+
+        JAEMIN: {
+            color: "#1bcf72"
+        },
+
+        JISUNG: {
+            color: "#df068c"
+        }
+
+    }
+
+};
