19e64a69daef9c669c96eb649862e775ee6cb584
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-12 18:41:19 +0200

    Add previousTime tracking to Engine update method

--- js/engine.js
+++ js/engine.js
@@ -6,10 +6,47 @@ Engine
 
 const Engine = {
 
+    previousTime:0,
+
     update(time){
+
+        const delta =
+
+            time - this.previousTime;
+
+        this.previousTime = time;
+
         Lyrics.update(time);
 
-        // Próximo sprint
+        const current =
+
+            SONG.lyrics.find(
+
+                line =>
+
+                time >= line.start &&
+
+                time < line.end
+
+            );
+
+        if(current){
+
+            Ranking.addTime(
+
+                current.member,
+
+                delta
+
+            );
+
+            Ranking.refresh(
+
+                SONG.duration
+
+            );
+
+        }
 
     }
 
