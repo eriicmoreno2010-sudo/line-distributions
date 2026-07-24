6aa64b4ab5df2673c08266e0463eff33f570950f
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-24 15:41:36 +0200

    P1Harmony: apply framed photo crops; bump PHOTO_VER to 3

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,8 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 2;
+const PHOTO_VER = 3;
+window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
 
