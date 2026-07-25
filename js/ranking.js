55f82d99e6038447b9ab9dddf6c6869bea32ab6d
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-25 19:32:42 +0200

    Sara Sara: apply framed + rotated photo crops; bump PHOTO_VER to 5

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 4;
+const PHOTO_VER = 5;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
