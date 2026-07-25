5c9a226067ecc2f4ce687d16e8b2cae00f86da4a
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-25 22:35:01 +0200

    ATEEZ BAD: apply framed photo crops; bump PHOTO_VER to 7

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 6;
+const PHOTO_VER = 7;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
