e4f35a6b4e01a6c7f4473b939fd148229da80f2b
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-25 19:36:42 +0200

    Sara Sara: swap Joshua photo (same framing); bump PHOTO_VER to 6

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 5;
+const PHOTO_VER = 6;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
