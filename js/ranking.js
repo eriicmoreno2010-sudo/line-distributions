2701ec9daeb4675353b0e6e74d6dc40b0a63ed6c
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-26 19:48:22 +0200

    TREASURE IF I: apply framed photo crops; bump PHOTO_VER to 10

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 9;
+const PHOTO_VER = 10;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
