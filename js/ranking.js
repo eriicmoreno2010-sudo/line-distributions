7ebdb80661abac4f58fe2c413984da57194eaf7d
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-26 17:09:09 +0200

    SEVENTEEN Shohikigen: frame the last 4 photos (mingyu/the8/seungkwan/vernon); bump PHOTO_VER to 9

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 8;
+const PHOTO_VER = 9;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
