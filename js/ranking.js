1bbb8d0006aeccd45f8234830f9b5a85d4ed78fe
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-26 14:52:51 +0200

    SEVENTEEN Shohikigen: frame 9 photos (4 pending); bump PHOTO_VER to 8

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 7;
+const PHOTO_VER = 8;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
