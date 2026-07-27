fce36da748e1f2073638fc890b8a8cdb1c99ee9b
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-27 20:06:28 +0200

    F*ck My Life: frame 12 member photos (all but Joshua); bump PHOTO_VER 17

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 16;
+const PHOTO_VER = 17;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
