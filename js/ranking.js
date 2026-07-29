4dbfc5587e2e84b7afe593e161446e255fc0be79
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-07-29 16:15:06 +0200

    Smoothie: add 7 member photos (4K originals in _src, 800px display placeholders); bump PHOTO_VER 24

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 23;
+const PHOTO_VER = 24;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
