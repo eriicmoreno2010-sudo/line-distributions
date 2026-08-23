0b17c7738f3768c887e5622f56fad72884f3dd13
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-08-23 12:01:39 +0200

    Update Golden Age photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 26;
+const PHOTO_VER = 27;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
