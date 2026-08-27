21215ec8c6112bf67d5f57ca4a961d9559e8d2a3
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-08-27 19:02:53 +0200

    Update DUH! photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 34;
+const PHOTO_VER = 35;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
