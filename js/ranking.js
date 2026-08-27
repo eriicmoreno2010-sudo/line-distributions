31e900f945b89f79eaa20bac71352ad5cee4a165
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-08-27 21:58:27 +0200

    Update 127 Million Miles photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 37;
+const PHOTO_VER = 38;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
