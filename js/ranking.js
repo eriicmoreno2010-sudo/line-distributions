67913b00a150ac41e7585ef051c5d85f937ec67e
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-08-28 16:01:58 +0200

    Update icantfeelanything photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 40;
+const PHOTO_VER = 41;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
