c922fba94fb00c395e2127f43864cd6b69a1ab46
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-08-31 15:39:23 +0200

    Update When I'm With You photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 41;
+const PHOTO_VER = 42;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
