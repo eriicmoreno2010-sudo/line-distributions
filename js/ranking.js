7e1de5c0645b2334691d4a2ac5aa28b693d2a2bf
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-09-02 21:02:58 +0200

    Update HBD photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 54;
+const PHOTO_VER = 55;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
