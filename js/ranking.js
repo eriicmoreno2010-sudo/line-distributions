855ad2026d33a0f36108167d3d40a01c16c486cb
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-09-17 00:11:17 +0200

    Update 숨 (Breathing) photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 71;
+const PHOTO_VER = 72;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
