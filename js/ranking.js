52513ba1638c74be5cb322c1ff6fdc856b19920b
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-09-17 22:51:11 +0200

    Cutout data/oneus/runaway.json (quitar fondo)

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 77;
+const PHOTO_VER = 78;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
