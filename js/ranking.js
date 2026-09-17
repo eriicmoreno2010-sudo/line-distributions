7983bea289539aa9681cc6168ca2f2eebd475658
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-09-17 18:44:53 +0200

    Cutout data/nctdream/breathing.json (quitar fondo)

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 75;
+const PHOTO_VER = 76;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
