9f0b13337f71cb39832f1ca23f72f1e131fc3942
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-09-22 13:08:56 +0200

    Update Shining Star photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 94;
+const PHOTO_VER = 95;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
