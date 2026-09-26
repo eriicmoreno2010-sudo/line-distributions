2c4632181cf3ab90bfe67be4f18008561875853a
eriicmoreno2010-sudo <eriicmoreno2010@gmail.com>
2026-09-26 18:51:35 +0200

    Update Moonlight photos

--- js/ranking.js
+++ js/ranking.js
@@ -4,7 +4,7 @@
 /* ========================================= */
 
 /* Bump when the avatar images change, to bust the browser/Pages cache. */
-const PHOTO_VER = 100;
+const PHOTO_VER = 101;
 window.PHOTO_VER = PHOTO_VER;
 
 const Ranking = {
